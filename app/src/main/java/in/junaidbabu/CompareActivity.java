e467328b929a2bdb9d29eec62be276eae7ff21d5
Junaid Babu <junaidvayoli@gmail.com>
2014-07-09 18:13:47 +0900

    Realtime Calc for time diff

--- app/src/main/java/in/junaidbabu/CompareActivity.java
+++ app/src/main/java/in/junaidbabu/CompareActivity.java
@@ -18,8 +18,8 @@ import mysc.CustomVideoView;
 public class CompareActivity extends YouTubeBaseActivity {
     YouTubePlayer player;
     CustomVideoView video;
-    TextView t1,t2;
-    String url="https://r1---sn-nvoxu-ioql.googlevideo.com/videoplayback?sver=3&mv=m&requiressl=yes&ipbits=0&itag=22&ip=182.171.224.146&sparams=gcr%2Cid%2Cip%2Cipbits%2Citag%2Cratebypass%2Crequiressl%2Csource%2Cupn%2Cexpire&fexp=902408%2C902531%2C914088%2C916612%2C917000%2C924222%2C930008%2C931014%2C934024%2C934030%2C936209%2C937427&upn=4YSB4Ua236Y&expire=1404198000&ms=au&id=o-ACa-e0QzVSL5yXk8KkpXzn6VOCLX_1rdR-Kg36XTupBy&mws=yes&key=yt5&mt=1404174281&gcr=jp&ratebypass=yes&source=youtube&signature=728C32076F3487B99F1E645149ACC36EAB1904.73B700F8B5F14A043B53EA5D139B6706F355BEC4";
+    TextView t1,t2, LT;
+    String url="https://r1---sn-nvoxu-ioql.googlevideo.com/videoplayback?mv=m&sver=3&requiressl=yes&mt=1404887139&id=o-AKAQ_2k3nJl5HSuce_w0z947gT7fIQMZ1livnDohbstp&upn=Ieb-vnPsYx8&expire=1404910800&ipbits=0&ratebypass=yes&gcr=jp&ms=au&mws=yes&itag=22&key=yt5&source=youtube&sparams=gcr%2Cid%2Cip%2Cipbits%2Citag%2Cratebypass%2Crequiressl%2Csource%2Cupn%2Cexpire&ip=182.171.224.146&fexp=902408%2C924213%2C924217%2C924222%2C930008%2C934024%2C934030&signature=8966A8106CD388F8BDA01F2D7C09261C98BFA2EF.DCCCF8514455D942DCE3B62DD443EC7B56B223C3";
     String vid="k4V3Mo61fJM";
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,12 +28,15 @@ public class CompareActivity extends YouTubeBaseActivity {
 
         t1=(TextView)findViewById(R.id.textView2);
         t2=(TextView)findViewById(R.id.textView3);
+        LT=(TextView)findViewById(R.id.textView4);
+
         // YouTubePlayerView youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
         //youTubeView.initialize(DeveloperKey.DEVELOPER_KEY, this);
         video= (CustomVideoView) findViewById(R.id.videoView);
 
-
-        video.setMediaController(new MediaController(this, false));
+        MediaController mediaController = new MediaController(this, false);
+        mediaController.setAnchorView(findViewById(R.id.videoView));
+        video.setMediaController(mediaController);
         YouTubePlayerView videoview = ((YouTubePlayerView) findViewById(R.id.youtube_view));
         videoview.initialize(DeveloperKey.DEVELOPER_KEY, new YouTubePlayer.OnInitializedListener() {
             @Override
@@ -114,6 +117,7 @@ public class CompareActivity extends YouTubeBaseActivity {
     public void stop(View v){
         t1.setText(String.valueOf((float)player.getCurrentTimeMillis()/1000));
         t2.setText(String.valueOf((float) video.getCurrentPosition() / 1000));
+        LT.setText(String.valueOf((float) video.getCurrentPosition() / 1000 - (float)player.getCurrentTimeMillis()/1000));
         player.pause();
         video.pause();
 
